ed6394aeb7cabb714d2bcd8f95547e7c85b273ca
ibgputragautama-code <ibgputragautama@gmail.com>
2025-12-03 15:32:39 +0800

    Membuat index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<?php require __DIR__ . '/inc/config.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CRUD Produk</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Home</h1>
+list.phpLihat Produk</a> | create.phpTambah Produk</a>
+<?php Utility::showFlash(); ?>
+</body>
+</html>
