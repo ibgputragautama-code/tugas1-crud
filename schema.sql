f00bfabc37de64a7a0d4be0594ce39be3c6e29c4
ibgputragautama-code <ibgputragautama@gmail.com>
2025-12-03 15:15:58 +0800

    Membuat schema.sql

---
+++ schema.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE IF NOT EXISTS crud_app;
+USE crud_app;
+
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock INT NOT NULL,
+    image_path VARCHAR(255),
+    status ENUM('active','inactive') DEFAULT 'active'
+);
